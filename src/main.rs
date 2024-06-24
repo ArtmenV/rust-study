c4c2f9ac95b00429daa20c7bc662c015133e18b9
ArtmenV <viliutich.artem@gmail.com>
2024-06-24 11:25:06 +0700

    operator match

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
 fn main() {
-    println!("Hello, world!1");
-    println!("Привет от RUST1");
+    let name = String::from("Artem");
+    println!("{}", name);
+
+    let age: i32 = 19;
+
+    if age >= 18 {
+        println!("Ты готов заходи")
+    }
+
+    let num = 24;
+
+    match num {
+        10 => println!("Num is 10"),
+        23 => {
+            println!("num is 23!");
+            println!("Num is matched!");
+        },
+        10..=50 => {
+            println!("diapason 50")
+        }
+        __ => {
+            println!("defaul value")
+        }
+    }
 }
